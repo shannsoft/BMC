1d82677f84d3a601aa6c2b2d34c3d43e244892b2
rajashree86 <biswal.rajasree86@gmail.om>
2016-07-29 14:43:06 +0530

    new employee added

--- admin/controller/main_controller.js
+++ admin/controller/main_controller.js
@@ -37,9 +37,56 @@ app.controller("Main_Controller",function($scope,$rootScope,$state,$localStorage
   /*******************************************************/
   /*************This is use for  user logout**************/
   /*******************************************************/
-  $scope.signOut = function(){
-    localStorage.setItem('accessToken','');
-    $rootScope.loggedin = false;
-    $state.go('login');
+  $scope.open2 = function() {
+   $scope.popup1.opened1 = true;
+  };
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+  $scope.format = $scope.formats[0];
+  $scope.altInputFormats = ['M!/d!/yyyy'];
+  $scope.popup1 = {
+    opened1: false
+  };
+
+  function getDayClass(data) {
+    var date = data.date,
+      mode = data.mode;
+    if (mode === 'day') {
+      var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+      for (var i = 0; i < $scope.events.length; i++) {
+        var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+        if (dayToCheck === currentDay) {
+          return $scope.events[i].status;
+        }
+      }
+    }
+    return '';
   }
+ $scope.open = function() {
+  $scope.popup2.opened = true;
+ };
+ $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+ $scope.format = $scope.formats[0];
+ $scope.altInputFormats = ['M!/d!/yyyy'];
+ $scope.popup2 = {
+   opened: false
+ };
+
+ function getDayClass(data) {
+   var date = data.date,
+     mode = data.mode;
+   if (mode === 'day') {
+     var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+     for (var i = 0; i < $scope.events.length; i++) {
+       var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+       if (dayToCheck === currentDay) {
+         return $scope.events[i].status;
+       }
+     }
+   }
+   return '';
+ }
 });
